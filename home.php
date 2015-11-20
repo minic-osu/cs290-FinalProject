e855261b533ea0a270fe15932a0956763552497b
ksliker89 <kevin.sliker@gmail.com>
2015-11-19 19:09:49 -0800

    building home page

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" href="">
+</head>
+<body>
+
+<div id="header">
+</div>
+
+<div id="navigation">
+<ul>
+<li>
+<a target="_self" href="index.php">Home</a>
+</li>
+<li>
+<a target="_self" href="about.php">About</a>
+</li>
+<li>
+<a target="_self" href="help.php">Help</a>
+</li>
+<li>
+<a target="_self" href="past_fortunes.php">Past Fortunes</a>
+</li>
+</ul>
+</div>
+
+
+</body>
+
+</html>

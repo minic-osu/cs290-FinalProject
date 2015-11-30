ee6476e237abeb3f9b270280ed8f200941c36ccd
Claudia Mini <minic@os-class.engr.oregonstate.edu>
2015-11-30 13:45:35 -0800

    Got rid of second nav bar on about page

--- about.php
+++ about.php
@@ -75,36 +75,14 @@ if ((isset($_POST['description'])) && (isset($_POST['major'])) && (isset($_POST[
 	  </ul>
 
 	  <ul class="nav navbar-nav navbar-right">
-	    <li><a href="#">Log Out</a></li>
+	    <li><a href="signOut.php">Log Out</a></li>
 
 	  </ul>
 	</div><!-- /.navbar-collapse -->
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Fortune Teller</a>
-        </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li><a href="homePage.php">Home <span class="sr-only">(current)</span></a></li>
-            <li class="active"><a href="about.php">About</a></li>
-            <li><a href="help.php">Help</a></li>
-            <li><a href="past_fortunes.php">Past Fortunes</a></li>
-           
-          </ul>
 
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="signOut.php">Log Out</a></li>
 
-          </ul>
-        </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
   </header>

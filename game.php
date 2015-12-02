4128076a2a11f2bfbac07ddbee16ec215f27b23e
kannabanana <kannas@oregonstate.edu>
2015-12-02 14:00:57 -0800

    game.php

--- game.php
+++ game.php
@@ -15,49 +15,481 @@ function playgame($conn,$username,$answer1,$answer2)
 	{
 		if($answer2 ==1)
 		{
-			$fortune = "Good things are going to start coming your way";
-			echo "<p>Your fortune is ".$fortune;
-			$query = "SELECT fortune_desc FROM fortune WHERE username = '$username'";
-			if(!$query)
-			{
-				echo"<p>Could not find the query in the database</p>";
-
-			}
-			else{	
-			$databasecon = mysqli_query($conn,$query);
-			$fortune_old = mysqli_fetch_array($databasecon);
-			//	if(!$fortune_old)
-			//		echo "no row";
-			//	else
-			//		echo "yes row".$fortune_old;
-
-			//concat string (old and new)
-			$fortune2 = $fortune_old['fortune_desc'];
-			$fortune2 .= $fortune;	//string concat
-			echo "<p>The new fortune is </p>".$fortune2;
+			$id_query = "SELECT id FROM student WHERE username = '$username'";
+			var_dump($id_query);
+			$id_query2 = mysqli_query($conn,$id_query);
+			var_dump($id_query2);
+			if(!$id_query2)
+				echo"<p>There is no id</p>";
 		
-				//UPDATE:
-				$final_score = "UPDATE Users SET fortune_desc=$fortune WHERE username='$username'";
-				if(!$final_score)
-					echo"<p>Could not update database with new fortune</p>";
+			$id_query3 = mysqli_fetch_array($id_query2);
+			$id_query4 = $id_query3['id'];
 
+			
+			$fortune = "Good things are going coming your way";
+			echo"<p>Your fortune is ".$fortune;
+
+			$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+			var_dump($new_query);
+			if(!$new_query)
+				echo"<p>No update to fortune description</p>";
+			else
+				$new_query2 = mysqli_query($conn,$new_query);
+			if(!$new_query2)
+				echo"<p>Final query didn't go through</p";
+
+			echo"<p>If you would like to play again, click play again!<p>";
+			echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+		}
+
+		else if($answer2 ==2)
+		{
+			$id_query = "SELECT id FROM student WHERE username = '$username'";
+			var_dump($id_query);
+			$id_query2 = mysqli_query($conn,$id_query);
+			var_dump($id_query2);
+			if(!$id_query2)
+				echo"<p>There is no id</p>";
+		
+			$id_query3 = mysqli_fetch_array($id_query2);
+			$id_query4 = $id_query3['id'];
+
+			
+			$fortune = "Good things are not coming your way";
+			echo"<p>Your fortune is ".$fortune;
+
+			$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+			var_dump($new_query);
+			if(!$new_query)
+				echo"<p>No update to fortune description</p>";
+			else
+				$new_query2 = mysqli_query($conn,$new_query);
+			if(!$new_query2)
+				echo"<p>Final query didn't go through</p";
+
+			echo"<p>If you would like to play again, click play again!<p>";
+			echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+		}
+
+		else if($answer2 ==3)
+		{
+			$id_query = "SELECT id FROM student WHERE username = '$username'";
+			var_dump($id_query);
+			$id_query2 = mysqli_query($conn,$id_query);
+			var_dump($id_query2);
+			if(!$id_query2)
+				echo"<p>There is no id</p>";
+		
+			$id_query3 = mysqli_fetch_array($id_query2);
+			$id_query4 = $id_query3['id'];
+
+			
+			$fortune = "You may want to reconsider the decision you're making";
+			echo"<p>Your fortune is ".$fortune;
+
+			$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+			var_dump($new_query);
+			if(!$new_query)
+				echo"<p>No update to fortune description</p>";
+			else
+				$new_query2 = mysqli_query($conn,$new_query);
+			if(!$new_query2)
+				echo"<p>Final query didn't go through</p";
+
+			echo"<p>If you would like to play again, click play again!<p>";
+			echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+		}
+
+		else
+		{
+			$id_query = "SELECT id FROM student WHERE username = '$username'";
+			var_dump($id_query);
+			$id_query2 = mysqli_query($conn,$id_query);
+			var_dump($id_query2);
+			if(!$id_query2)
+				echo"<p>There is no id</p>";
+		
+			$id_query3 = mysqli_fetch_array($id_query2);
+			$id_query4 = $id_query3['id'];
+
+			
+			$fortune = "Be close to those who you love...they may need you";
+			echo"<p>Your fortune is ".$fortune;
+
+			$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+			var_dump($new_query);
+			if(!$new_query)
+				echo"<p>No update to fortune description</p>";
+			else
+				$new_query2 = mysqli_query($conn,$new_query);
+			if(!$new_query2)
+				echo"<p>Final query didn't go through</p";
+
+			echo"<p>If you would like to play again, click play again!<p>";
+			echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+		}
+
+	}
+	else if($answer1==2)
+	{
+
+		if($answer2 ==1)
+		{
+			$id_query = "SELECT id FROM student WHERE username = '$username'";
+			var_dump($id_query);
+			$id_query2 = mysqli_query($conn,$id_query);
+			var_dump($id_query2);
+			if(!$id_query2)
+				echo"<p>There is no id</p>";
+		
+			$id_query3 = mysqli_fetch_array($id_query2);
+			$id_query4 = $id_query3['id'];
+
+			
+			$fortune = "Watch your back";
+			echo"<p>Your fortune is ".$fortune;
+
+			$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+			var_dump($new_query);
+			if(!$new_query)
+				echo"<p>No update to fortune description</p>";
+			else
+				$new_query2 = mysqli_query($conn,$new_query);
+			if(!$new_query2)
+				echo"<p>Final query didn't go through</p";
+
+			echo"<p>If you would like to play again, click play again!<p>";
+			echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+		}
+
+		else if($answer2 ==2)
+		{
+			$id_query = "SELECT id FROM student WHERE username = '$username'";
+			var_dump($id_query);
+			$id_query2 = mysqli_query($conn,$id_query);
+			var_dump($id_query2);
+			if(!$id_query2)
+				echo"<p>There is no id</p>";
+		
+			$id_query3 = mysqli_fetch_array($id_query2);
+			$id_query4 = $id_query3['id'];
+
+			
+			$fortune = "Pay attention to your surroundings";
+			echo"<p>Your fortune is ".$fortune;
+
+			$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+			var_dump($new_query);
+			if(!$new_query)
+				echo"<p>No update to fortune description</p>";
+			else
+				$new_query2 = mysqli_query($conn,$new_query);
+			if(!$new_query2)
+				echo"<p>Final query didn't go through</p";
+
+			echo"<p>If you would like to play again, click play again!<p>";
+			echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+		}
+
+		else if($answer2 ==3)
+		{
+			$id_query = "SELECT id FROM student WHERE username = '$username'";
+			var_dump($id_query);
+			$id_query2 = mysqli_query($conn,$id_query);
+			var_dump($id_query2);
+			if(!$id_query2)
+				echo"<p>There is no id</p>";
+		
+			$id_query3 = mysqli_fetch_array($id_query2);
+			$id_query4 = $id_query3['id'];
+
+			
+			$fortune = "Be generous to those who have less and considerate to those who have more...generosity is a good look for you";
+			echo"<p>Your fortune is ".$fortune;
+
+			$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+			var_dump($new_query);
+			if(!$new_query)
+				echo"<p>No update to fortune description</p>";
+			else
+				$new_query2 = mysqli_query($conn,$new_query);
+			if(!$new_query2)
+				echo"<p>Final query didn't go through</p";
+
+			echo"<p>If you would like to play again, click play again!<p>";
+			echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+		}
+
+		else
+		{
+			$id_query = "SELECT id FROM student WHERE username = '$username'";
+			var_dump($id_query);
+			$id_query2 = mysqli_query($conn,$id_query);
+			var_dump($id_query2);
+			if(!$id_query2)
+				echo"<p>There is no id</p>";
+		
+			$id_query3 = mysqli_fetch_array($id_query2);
+			$id_query4 = $id_query3['id'];
+
+			
+			$fortune = "Be considerate to those around you ";
+			echo"<p>Your fortune is ".$fortune;
+
+			$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+			var_dump($new_query);
+			if(!$new_query)
+				echo"<p>No update to fortune description</p>";
+			else
+				$new_query2 = mysqli_query($conn,$new_query);
+			if(!$new_query2)
+				echo"<p>Final query didn't go through</p";
+
+			echo"<p>If you would like to play again, click play again!<p>";
+			echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+		}
+
+		else if($answer1 ==3)
+		{
+			if($answer2 ==1)
+			{
+				$id_query = "SELECT id FROM student WHERE username = '$username'";
+				var_dump($id_query);
+				$id_query2 = mysqli_query($conn,$id_query);
+				var_dump($id_query2);
+				if(!$id_query2)
+					echo"<p>There is no id</p>";
+
+				$id_query3 = mysqli_fetch_array($id_query2);
+				$id_query4 = $id_query3['id'];
+
+
+				$fortune = "Today you will eat something less than $10";
+				echo"<p>Your fortune is ".$fortune;
+
+				$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+				var_dump($new_query);
+				if(!$new_query)
+					echo"<p>No update to fortune description</p>";
 				else
-					$result = mysqli_query($conn,$final_score);
+					$new_query2 = mysqli_query($conn,$new_query);
+				if(!$new_query2)
+					echo"<p>Final query didn't go through</p";
 
 				echo"<p>If you would like to play again, click play again!<p>";
 				echo '<a href="homePage.php" target="blank" >Play Again</a>';  
-
 			}
 
-		
+			else if($answer2 ==2)
+			{
+				$id_query = "SELECT id FROM student WHERE username = '$username'";
+				var_dump($id_query);
+				$id_query2 = mysqli_query($conn,$id_query);
+				var_dump($id_query2);
+				if(!$id_query2)
+					echo"<p>There is no id</p>";
+
+				$id_query3 = mysqli_fetch_array($id_query2);
+				$id_query4 = $id_query3['id'];
+
+
+				$fortune = "The effort you put in may not be recognized";
+				echo"<p>Your fortune is ".$fortune;
+
+				$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+				var_dump($new_query);
+				if(!$new_query)
+					echo"<p>No update to fortune description</p>";
+				else
+					$new_query2 = mysqli_query($conn,$new_query);
+				if(!$new_query2)
+					echo"<p>Final query didn't go through</p";
+
+				echo"<p>If you would like to play again, click play again!<p>";
+				echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+			}
+
+			else if($answer2 ==3)
+			{
+				$id_query = "SELECT id FROM student WHERE username = '$username'";
+				var_dump($id_query);
+				$id_query2 = mysqli_query($conn,$id_query);
+				var_dump($id_query2);
+				if(!$id_query2)
+					echo"<p>There is no id</p>";
+
+				$id_query3 = mysqli_fetch_array($id_query2);
+				$id_query4 = $id_query3['id'];
+
+
+				$fortune = "You may need to spend more time on yourself";
+				echo"<p>Your fortune is ".$fortune;
+
+				$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+				var_dump($new_query);
+				if(!$new_query)
+					echo"<p>No update to fortune description</p>";
+				else
+					$new_query2 = mysqli_query($conn,$new_query);
+				if(!$new_query2)
+					echo"<p>Final query didn't go through</p";
+
+				echo"<p>If you would like to play again, click play again!<p>";
+				echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+			}
+
+			else
+			{
+				$id_query = "SELECT id FROM student WHERE username = '$username'";
+				var_dump($id_query);
+				$id_query2 = mysqli_query($conn,$id_query);
+				var_dump($id_query2);
+				if(!$id_query2)
+					echo"<p>There is no id</p>";
+
+				$id_query3 = mysqli_fetch_array($id_query2);
+				$id_query4 = $id_query3['id'];
+
+
+				$fortune = "The things you want may not be the things you need";
+				echo"<p>Your fortune is ".$fortune;
+
+				$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+				var_dump($new_query);
+				if(!$new_query)
+					echo"<p>No update to fortune description</p>";
+				else
+					$new_query2 = mysqli_query($conn,$new_query);
+				if(!$new_query2)
+					echo"<p>Final query didn't go through</p";
+
+				echo"<p>If you would like to play again, click play again!<p>";
+				echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+			}
+		else{
+
+			if($answer2 ==1)
+			{
+				$id_query = "SELECT id FROM student WHERE username = '$username'";
+				var_dump($id_query);
+				$id_query2 = mysqli_query($conn,$id_query);
+				var_dump($id_query2);
+				if(!$id_query2)
+					echo"<p>There is no id</p>";
+
+				$id_query3 = mysqli_fetch_array($id_query2);
+				$id_query4 = $id_query3['id'];
+
+
+				$fortune = "The lesser of the two evils is still evil";
+				echo"<p>Your fortune is ".$fortune;
+
+				$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+				var_dump($new_query);
+				if(!$new_query)
+					echo"<p>No update to fortune description</p>";
+				else
+					$new_query2 = mysqli_query($conn,$new_query);
+				if(!$new_query2)
+					echo"<p>Final query didn't go through</p";
+
+				echo"<p>If you would like to play again, click play again!<p>";
+				echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+			}
+
+			else if($answer2 ==2)
+			{
+				$id_query = "SELECT id FROM student WHERE username = '$username'";
+				var_dump($id_query);
+				$id_query2 = mysqli_query($conn,$id_query);
+				var_dump($id_query2);
+				if(!$id_query2)
+					echo"<p>There is no id</p>";
+
+				$id_query3 = mysqli_fetch_array($id_query2);
+				$id_query4 = $id_query3['id'];
+
+
+				$fortune = "You will face regret and dissapointment once again";
+				echo"<p>Your fortune is ".$fortune;
+
+				$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+				var_dump($new_query);
+				if(!$new_query)
+					echo"<p>No update to fortune description</p>";
+				else
+					$new_query2 = mysqli_query($conn,$new_query);
+				if(!$new_query2)
+					echo"<p>Final query didn't go through</p";
+
+				echo"<p>If you would like to play again, click play again!<p>";
+				echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+			}
+
+			else if($answer2 ==3)
+			{
+				$id_query = "SELECT id FROM student WHERE username = '$username'";
+				var_dump($id_query);
+				$id_query2 = mysqli_query($conn,$id_query);
+				var_dump($id_query2);
+				if(!$id_query2)
+					echo"<p>There is no id</p>";
+
+				$id_query3 = mysqli_fetch_array($id_query2);
+				$id_query4 = $id_query3['id'];
+
+
+				$fortune = "You will be happy and sad many times in your life but never alone";
+				echo"<p>Your fortune is ".$fortune;
+
+				$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+				var_dump($new_query);
+				if(!$new_query)
+					echo"<p>No update to fortune description</p>";
+				else
+					$new_query2 = mysqli_query($conn,$new_query);
+				if(!$new_query2)
+					echo"<p>Final query didn't go through</p";
+
+				echo"<p>If you would like to play again, click play again!<p>";
+				echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+			}
+
+			else
+			{
+				$id_query = "SELECT id FROM student WHERE username = '$username'";
+				var_dump($id_query);
+				$id_query2 = mysqli_query($conn,$id_query);
+				var_dump($id_query2);
+				if(!$id_query2)
+					echo"<p>There is no id</p>";
+
+				$id_query3 = mysqli_fetch_array($id_query2);
+				$id_query4 = $id_query3['id'];
+
+
+				$fortune = "Those around you have a deep influence over you";
+				echo"<p>Your fortune is ".$fortune;
+
+				$new_query = "INSERT INTO fortune (student_id,fortune_desc) VALUES('$id_query4','$fortune')";
+				var_dump($new_query);
+				if(!$new_query)
+					echo"<p>No update to fortune description</p>";
+				else
+					$new_query2 = mysqli_query($conn,$new_query);
+				if(!$new_query2)
+					echo"<p>Final query didn't go through</p";
+
+				echo"<p>If you would like to play again, click play again!<p>";
+				echo '<a href="homePage.php" target="blank" >Play Again</a>';  
+			}
 
 
 		}
 
 
-
-	}	
 }
+	
 
 
 

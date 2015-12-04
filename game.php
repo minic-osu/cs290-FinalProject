b429432981eccced769abdc29059939d89e20b9d
kannabanana <kannas@oregonstate.edu>
2015-12-03 16:57:38 -0800

    game.php

--- game.php
+++ game.php
@@ -1,3 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+	
+	<title>Sign-In</title>
+		<link rel="stylesheet" href="css/bootstrap.css">
+  		<link rel="stylesheet" href="css/bootstrap-responsive.css">
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	</head>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Fortune Teller</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="signOut.php">Sign Out</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    	<div id="wrapper">
+
+		<header id="header">
+		</header><!-- #header -->
+
+
 <?php
 session_start();
 include 'connectivity.php';
@@ -524,4 +558,17 @@ else
 mysqli_close($conn);
 
 
-?>
+?> 
+
+	<p>Click past fortunes to see past fortunes!</p>
+	</body>
+<footer>
+    <nav class="navbar navbar-default navbar-bottom" role="navigation">
+      <div class="container">
+      </div>
+    </nav>
+  </footer>
+
+
+</body>
+</html>  

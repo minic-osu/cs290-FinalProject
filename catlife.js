ee1c0e801d7b8e4426fd200246d9e00f4b848601
kannabanana <kannas@oregonstate.edu>
2015-12-04 17:10:07 -0800

    java

---
+++ catlife.js
@@ -0,0 +1,43 @@
+function display(num)
+{
+
+	document.getElementById("result").innerHTML = ("You will live "+num+" years.");
+}
+
+
+function random(){
+	var x = Math.floor((math.random()*99)+1);	
+	return x;
+	}
+
+
+function compute(num)
+{
+	var x = random();
+	if(num == 1)
+	{
+		display(x);
+	}
+	elseif(num ==2)
+	{
+		x+=x;
+		display(x);
+	}
+	eleif(num==3)
+	{
+		x=*7;
+		display(x);
+	}
+	else{
+		display(x);
+	}
+}
+
+
+
+
+
+document.getElementbyId("Image")=function() {compute(1)};
+document.getElementbyId("Image2")=function(){compute(2)};
+document.getElementbyId("Image3")=function(){compute(3)};
+document.getElementbyId("Image4")=function(){compute(4)};
